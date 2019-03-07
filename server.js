6041105762ea027466d042b167c22f4d0811c50f
sultan <aidaraliuulu@gmail.com>
2019-03-07 19:33:24 +0600

    Text server.js get end post

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 1000;
+const port = 8000;
 
 
+app.get('/products', (req, res) => {
+    res.send('List of products will be here');
+});
+app.get('/products/:id', (req, res) => {
+    res.send('A single product by id will be here');
+});
+app.post('/products', (req, res) => {
+    res.send('Will create new product here');
+});
 
 app.listen(port, () => {
     console.log(`Server started on${port} port!`);
